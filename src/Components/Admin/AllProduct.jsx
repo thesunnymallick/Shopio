cb4803c117c35da7ba76ce91af91c3b1dc20de2e
Alfe Sunny Mallick <81466855+thesunnymallick@users.noreply.github.com>
2023-03-29 19:33:19 +0530

    fix frontend

--- src/Components/Admin/AllProduct.jsx
+++ src/Components/Admin/AllProduct.jsx
@@ -41,7 +41,7 @@ function AllProduct() {
 
   // Pagination
   const [currentPage, setCurrentPage] = useState(1);
-  const [perPage] = useState(3);
+  const [perPage] = useState(6);
 
   const IndexOfLastProduct = currentPage * perPage;
   const IndexOfFirstProduct = IndexOfLastProduct - perPage;
